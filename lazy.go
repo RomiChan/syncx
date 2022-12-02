5f842c53020ea02cb60e508e36b52e42746a1b0b
wdvxdr1123 <wdvxdr1123@gmail.com>
2022-12-02 13:57:24 +0800

    add new api Lazy

---
+++ lazy.go
@@ -0,0 +1,40 @@
+// Copyright 2022 The syncx Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package syncx
+
+import (
+	"runtime"
+	"sync/atomic"
+)
+
+type Lazy[T any] struct {
+	done   atomic.Uint32
+	locked atomic.Uint32
+	val    T
+
+	Init func() T
+}
+
+func (l *Lazy[T]) Get() T {
+	if l.done.Load() == 1 {
+		return l.val
+	}
+	// Slow path (outlined so that the fast path can be inlined)
+	return l.slowGet()
+}
+
+func (l *Lazy[T]) slowGet() T {
+	// use a spinlock
+	for !l.locked.CompareAndSwap(0, 1) {
+		runtime.Gosched()
+	}
+	defer l.locked.Store(1)
+
+	if l.done.Load() == 0 {
+		defer l.done.Store(1)
+		l.val = l.Init()
+	}
+	return l.val
+}
---
+++ lazy_test.go
@@ -0,0 +1,42 @@
+// Copyright 2022 The syncx Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package syncx
+
+import "testing"
+
+var one = Lazy[int]{
+	Init: func() int {
+		return 1
+	},
+}
+
+func run(t *testing.T, c chan bool) {
+	if v := one.Get(); v != 1 {
+		t.Errorf("lazy failed inside run: %d is not 1", v)
+	}
+	c <- true
+}
+
+func TestLazy(t *testing.T) {
+	c := make(chan bool)
+	const N = 20
+	for i := 0; i < N; i++ {
+		go run(t, c)
+	}
+	for i := 0; i < N; i++ {
+		<-c
+	}
+	if one.val != 1 {
+		t.Errorf("lazy failed outside run: %d is not 1", one.val)
+	}
+}
+
+func BenchmarkLazy(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			one.Get()
+		}
+	})
+}
